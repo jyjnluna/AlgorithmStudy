5bd8a9e3778f8e559370cc1a0bfe27541fb2721c
Youngju Je <68914587+jyjnluna@users.noreply.github.com>
2022-04-03 16:53:19 +0900

    백준 알고리즘 10953번 문제풀이
    백준 알고리즘 10953번 문제를 자바로 풀었습니다.

---
+++ Baekjoon10953.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjoon10953 {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+			String[] str = sc.next().split(",");
+			System.out.println(Integer.parseInt(str[0])+Integer.parseInt(str[1]));
+		}
+	}
+
+}
